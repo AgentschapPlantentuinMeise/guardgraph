760dc08c73e30c5ae80cb3daa16f272425f68276
Christophe Van Neste <christophe.vanneste@plantentuinmeise.be>
2024-07-18 10:43:03 +0200

    only download data if /data/globi not yet created

--- guardgraph/__main__.py
+++ guardgraph/__main__.py
@@ -1,7 +1,9 @@
 def main():
     import os
     from guardgraph.graph import InteractionsGraph
-    os.mkdir('/data/globi')
-    ig = InteractionsGraph()
-    ig.retrieve_data_files()
-    ig.prep_admin_import_files()
+    # If you need to re-initiate delete/move dir /data/globi
+    if not os.path.exists('/data/globi'):
+        os.mkdir('/data/globi')
+        ig = InteractionsGraph()
+        ig.retrieve_data_files()
+        ig.prep_admin_import_files()
