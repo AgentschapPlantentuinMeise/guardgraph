47b66d201835e7740f1f54a374c94e91c62105db
Christophe Van Neste <christophe.vanneste@plantentuinmeise.be>
2024-07-22 17:32:55 +0200

    extra check if prep ran

--- guardgraph/__main__.py
+++ guardgraph/__main__.py
@@ -3,7 +3,10 @@ def main():
     from guardgraph.graph import InteractionsGraph
     # If you need to re-initiate delete/move dir /data/globi
     if not os.path.exists('/data/globi'):
+        print('Creating /data/globi')
         os.mkdir('/data/globi')
+    if not os.path.exists('/data/globi/interactions.tsv.gz'):
+        print('Downloading and preparing GloBI files')
         ig = InteractionsGraph()
         ig.retrieve_data_files()
         ig.prep_admin_import_files()
