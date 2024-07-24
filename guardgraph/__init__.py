9c31843ccf984a392cbfefb71387c36f5e9a8f84
Christophe Van Neste <christophe.vanneste@plantentuinmeise.be>
2024-07-24 16:02:09 +0200

    init function for when neo4j db is ready

--- guardgraph/__init__.py
+++ guardgraph/__init__.py
@@ -1,3 +1,5 @@
+import os
+import datetime
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,3 +7,17 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return 'Hello Interactions Seeker'
+
+@app.route('/init')
+def init_server():
+    # This entrypoint only runs once and should be called when the db is ready
+    if os.path.exists('INITIATED'):
+        return 'Already initiated'
+    else:
+        from guardgraph.graph import InteractionsGraph
+        with open('INITIATED','wt') as fout:
+            fout.write(str(datetime.datetime.now()))
+        ig = InteractionsGraph()
+        ig.set_random_password()
+        # Test and return
+        return str(ig.relationships)
