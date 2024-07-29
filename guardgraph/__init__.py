98c94138d190db6c4a3391f4a60c647308d1c4c9
Christophe Van Neste <christophe.vanneste@plantentuinmeise.be>
2024-07-29 13:44:10 +0200

    initial API routes

--- guardgraph/__init__.py
+++ guardgraph/__init__.py
@@ -1,6 +1,6 @@
 import os
 import datetime
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -21,3 +21,61 @@ def init_server():
         ig.set_random_password()
         # Test and return
         return str(ig.relationships)
+
+@app.route('/species', methods=['POST'])
+def describe_species():
+    species = request.get_json()
+    data = {
+        s: ig.run_query(
+            'MATCH (n:species)-[r]-() WHERE n.name STARTS WITH "'
+            +s.split()[0]+'" RETURN COUNT(r)'
+        )
+        for s in species
+    }
+    return jsonify(data)
+
+@app.route('/species/interactions', methods=['POST'])
+def get_interactions():
+    species = request.get_json()
+    data = query_interactions(species)
+    return jsonify(data)
+
+def query_interactions(species):
+    ig = InteractionsGraph()
+    data = {
+        s: ig.run_query(
+            'MATCH (n:species)-[r]-(m:species) WHERE n.name STARTS WITH "'
+            +s.split()[0]+'" RETURN DISTINCT m'
+        )
+        for s in species
+    }
+    return data
+
+
+@app.route('/species/interactors/cube', methods=['POST'])
+def get_interactors_cube():
+    from pygbif import species, occurrences
+    from guardgraph.gfib import cube_query
+    input_data = request.get_json()
+    species = input_data['species']
+    interactors = query_interactions(species)
+    speciesKeyList = set([
+        species.name_suggest(
+            s, limit=1
+        )[0]['speciesKey'] for s in species
+    ]+[
+        species.name_suggest(
+            s, limit=1
+        )[0]['speciesKey'] for s in interactors        
+    ])
+    cube_job_id = cube_query(
+        input_data['email'], input_data['gbif_user'],
+        input_data['gbif_pwd'], country, speciesKeyList
+    )
+    return jsonify({'cube_job_id': cube_job_id})
+
+@app.route('/species/interactors/cube/<cube_job_id>', methods=['GET'])
+def download_interactors_cube(cube_job_id):
+    from guardgraph.gfib import download_cube
+    download_cube(cube_job_id, prefix='/data/cubes')
+    return 'TODO pass download link'
---
+++ guardgraph/gbif.py
@@ -0,0 +1,103 @@
+"Copied from https://github.com/AgentschapPlantentuinMeise/irowine/blob/main/interactions/ixro/query.py"
+
+import os
+import json
+import time
+from urllib.request import urlretrieve
+import requests
+
+query_template = '''
+{{
+  "sendNotification": true,
+  "notificationAddresses": [
+    "{email}"
+  ],
+  "format": "SQL_TSV_ZIP",
+  "sql": "SELECT \
+    PRINTF('%04d-%02d', \\"year\\", \\"month\\") AS yearMonth,\
+    GBIF_EEARGCode(\
+      1000,\
+      decimalLatitude,\
+      decimalLongitude,\
+      COALESCE(coordinateUncertaintyInMeters, 1000)\
+      ) AS eeaCellCode,\
+    familyKey,\
+    family,\
+    speciesKey,\
+    species,\
+    datasetKey,\
+    COALESCE(sex, 'NOT_SUPPLIED') AS sex,\
+    COALESCE(occurrence.lifestage.concept, 'NOT_SUPPLIED') AS lifestage, \
+    COUNT(*) AS occurrences\
+  FROM\
+    occurrence\
+  WHERE\
+    geometry = '{geometry}'\
+    AND occurrenceStatus = 'PRESENT'\
+    AND \\"year\\" >= 2000\
+    AND hasCoordinate = TRUE\
+    AND speciesKey IN ({speciesKeyList})\
+    AND NOT ARRAY_CONTAINS(issue, 'ZERO_COORDINATE')\
+    AND NOT ARRAY_CONTAINS(issue, 'COORDINATE_OUT_OF_RANGE')\
+    AND NOT ARRAY_CONTAINS(issue, 'COORDINATE_INVALID')\
+    AND NOT ARRAY_CONTAINS(issue, 'COUNTRY_COORDINATE_MISMATCH')\
+    AND \\"month\\" IS NOT NULL\
+  GROUP BY\
+    yearMonth,\
+    datasetKey,\
+    eeaCellCode,\
+    familyKey,\
+    family,\
+    speciesKey,\
+    species,\
+    sex,\
+    lifestage\
+  ORDER BY\
+    yearMonth DESC,\
+    eeaCellCode ASC,\
+    speciesKey ASC;\
+    "
+}}
+'''
+
+def cube_query(email, gbif_user, gbif_pwd, geometry, speciesKeyList, query_template=query_template):
+    query = query_template.format(
+        email=email, geometry=geometry,
+        speciesKeyList=','.join(speciesKeyList)
+    )
+    #with open('query.json','wt') as qf:
+    #    qf.write(query)
+    headers = {
+        'Content-type': 'application/json',
+        #'Accept': 'text/plain'
+    }
+    r_valid = requests.post(
+        'https://api.gbif.org/v1/occurrence/download/request/validate',
+        data=query, headers=headers
+    )
+    if r_valid.status_code > 201:
+        raise Exception(r_valid.status_code)
+    r_cube = requests.post(
+        'https://api.gbif.org/v1/occurrence/download/request',
+        data=query, headers=headers,
+        auth = (gbif_user, gbif_pwd)
+    )
+    if r_cube.status_code > 201:
+        raise Exception(r_cube.status_code)
+    return r_cube.text # cube job id
+
+def download_cube(cube_job_id, prefix):
+    while (r:=json.loads(requests.get(
+        f"https://api.gbif.org/v1/occurrence/download/{cube_job_id}"
+    ).text))['status'] == 'RUNNING':
+        time.sleep(60)
+    if r['status'] != 'SUCCEEDED':
+        raise Exception(r['status'])
+    urlretrieve(
+        r['downloadLink'],
+        prefix+r['downloadLink'][r['downloadLink'].rindex('/')+1:]
+    )
+
+#curl --include --header "Content-Type: application/json" --data @query.json https://api.gbif.org/v1/occurrence/download/request/validate
+
+#curl --include --user 'maxime_ryckewaert':'password' --header "Content-Type: application/json" --data @query.json https://api.gbif.org/v1/occurrence/download/request
